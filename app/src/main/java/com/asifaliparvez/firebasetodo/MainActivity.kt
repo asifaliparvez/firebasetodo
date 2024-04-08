270271d7325018a5c5e7328297bc7c6244897167
asifaliparvez <asifaliparvezasif@gmail.com>
2024-04-08 20:22:22 +0530

    Second commit

--- app/src/main/java/com/asifaliparvez/firebasetodo/MainActivity.kt
+++ app/src/main/java/com/asifaliparvez/firebasetodo/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val name = "Asif"
 
         // Write a message to the database
         val database = Firebase.database
